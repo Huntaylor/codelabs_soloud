544473012ce19bddfd2a6a8f76df649b7739ba0c
Taylor Hunt <62562348+Airamentis@users.noreply.github.com>
2024-05-21 18:04:20 -0700

    step 5 changes

--- lib/audio/audio_controller.dart
+++ lib/audio/audio_controller.dart
@@ -8,6 +8,8 @@ class AudioController {
 
   SoLoud? _soLoud;
 
+  SoundHandle? _musicHandle;
+
   Future<void> initialize() async {
     _soLoud = SoLoud.instance;
     await _soLoud!.init();
@@ -27,11 +29,37 @@ class AudioController {
   }
 
   Future<void> startMusic() async {
-    _log.warning('Not implemented yet.');
+    if (_musicHandle != null) {
+      if (_soLoud!.getIsValidVoiceHandle(_musicHandle!)) {
+        _log.info('Music is already playing. Stopping first.');
+        await _soLoud!.stop(_musicHandle!);
+      }
+    }
+    final musicSource = await _soLoud!.loadAsset(
+      'assets/music/looped-song.ogg',
+      mode: LoadMode.disk,
+    );
+    musicSource.allInstancesFinished.first.then((_) {
+      _soLoud!.disposeSource(musicSource);
+      _log.info('Music source disposed');
+      _musicHandle = null;
+    });
+    _musicHandle = await _soLoud!.play(
+      musicSource,
+      volume: 0.6,
+      looping: true,
+      loopingStartAt: const Duration(seconds: 25, milliseconds: 43),
+    );
   }
 
   void fadeOutMusic() {
-    _log.warning('Not implemented yet.');
+    if (_musicHandle == null) {
+      _log.info('Nothing to fade out');
+      return;
+    }
+    const length = Duration(seconds: 5);
+    _soLoud!.fadeVolume(_musicHandle!, 0, length);
+    _soLoud!.scheduleStop(_musicHandle!, length);
   }
 
   void applyFilter() {
